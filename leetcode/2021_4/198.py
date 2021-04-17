d55e694bbcee2b50a299a7e3b071c63b16fbbed6
rundun159 <xogud1231@gmail.com>
2021-04-17 21:06:23 +0900

    DP Bottom Up & Top Down 198.py

--- leetcode/2021_4/198.py
+++ leetcode/2021_4/198.py
@@ -2,9 +2,9 @@ class TH:
     def __init__(self, nums):
         self.nums = nums
         self.len = len(nums)
-        self.cache = [-1 for i in nums]
 
     def dp(self, idx):
+        self.cache = [-1 for i in self.nums]
         if idx >= self.len:
             return 0
         if self.cache[idx] != -1:
@@ -15,13 +15,21 @@ class TH:
         self.cache[idx] = ret
         return ret
 
+    def top_down_dp(self):
+        self.cache2 = [None for i in self.nums + [None]]
+        self.cache2[self.len] = 0
+        self.cache2[self.len - 1] = self.nums[-1]
+        for idx in range(self.len - 2, -1, -1):
+            self.cache2[idx] = max(self.cache2[idx + 1], self.cache2[idx + 2] + self.nums[idx])
+        return self.cache2[0]
+
 
 class Solution:
     def rob(self, nums) -> int:
         th = TH(nums)
-        for i in range(th.len):
-            th.dp(i)
-        ret = -1
-        for i in th.cache:
-            ret = max(ret, i)
-        return ret
+        # for i in range(th.len):
+        #     th.dp(i)
+        # ret = -1
+        # for i in th.cache:
+        #     ret = max(ret, i)
+        return th.top_down_dp()
